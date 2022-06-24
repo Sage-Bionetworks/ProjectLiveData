eecf795c5c746083d8e855561a3996b801f642f1
andrewelamb <andrewelamb@gmail.com>
2022-06-24 10:50:55 -0700

    moved auth token to config file

--- inst/create_HTAN_RDS.R
+++ inst/create_HTAN_RDS.R
@@ -1,8 +1,10 @@
+config <- yaml::read_yaml(system.file("config.yaml", package = "ProjectLiveData"))
+
 obj <- create_projectlive_data_object(
   fileview_id = "syn20446927",
-  auth_token = .GlobalEnv$AUTH_TOKEN,
+  auth_token = config$auth_token,
   upload_to_synapse = T,
-  synapse_destination_id = "syn32204059"
+  synapse_destination_id = "syn32206991"
 )
 print(obj$get_project_manifest_errors)
 print(obj$synapse_get_manifest_errors)
--- tests/testthat/test-z-integration-tests.R
+++ tests/testthat/test-z-integration-tests.R
@@ -1,16 +1,22 @@
+config <- yaml::read_yaml(system.file("config.yaml", package = "ProjectLiveData"))
+
+expected_object_names <- c(
+  "projectlive_data_object",
+  "get_project_manifest_errors",
+  "synapse_get_manifest_errors"
+)
+
 test_that("create_projectlive_data_object", {
   obj <- create_projectlive_data_object(
     fileview_id = "syn20446927",
     project_ids_to_keep = "syn20834712",
     upload_to_synapse = T,
     synapse_destination_id = "syn32204059",
-    auth_token = AUTH_TOKEN
+    auth_token = config$auth_token
   )
-  expect_named(
-    obj,
-    c("projectlive_data_object", "get_project_manifest_errors", "synapse_get_manifest_errors")
-  )
+  expect_named(obj, expected_object_names)
   expect_named(obj$projectlive_data_object)
+  expect_true(length(obj$projectlive_data_object) > 0)
   expect_null(obj$get_project_manifest_errors)
   expect_null(obj$synapse_get_manifest_errors)
 })
