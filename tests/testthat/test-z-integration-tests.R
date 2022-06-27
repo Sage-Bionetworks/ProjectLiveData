f2330287886784d2942c315ffbb53bc252232251
andrewelamb <andrewelamb@gmail.com>
2022-06-27 07:56:15 -0700

    test fixes for githib workflow

--- tests/testthat/test-z-integration-tests.R
+++ tests/testthat/test-z-integration-tests.R
@@ -1,9 +1,9 @@
-print(list.files())
-print(Sys.getenv())
+auth_token = Sys.getenv("SYNAPSE_AUTH_TOKEN")
 
-config <- yaml::read_yaml(
-  system.file("config.yaml", package = "ProjectLiveData")
-)
+if (auth_token  == "") {
+  config <- yaml::read_yaml("configs/config.yaml")
+  auth_token = config$auth_token
+}
 
 expected_object_names <- c(
   "projectlive_data_object",
@@ -17,11 +17,12 @@ test_that("create_projectlive_data_object", {
     project_ids_to_keep = "syn20834712",
     upload_to_synapse = TRUE,
     synapse_destination_id = "syn32204059",
-    auth_token = config$auth_token
+    auth_token = auth_token
   )
   expect_named(obj, expected_object_names)
   expect_named(obj$projectlive_data_object)
   expect_true(length(obj$projectlive_data_object) > 0)
   expect_null(obj$get_project_manifest_errors)
   expect_null(obj$synapse_get_manifest_errors)
+  file.remove("data.RDS")
 })
